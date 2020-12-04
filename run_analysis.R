20c6a09049c71e64a69ed721da7bf96417f1d77c
jhellewell14 <lshjh2@lshtm.ac.uk>
2020-12-04 12:14:08 +0000

    Changes: - Added code to write csv files of curves - Added code to reproduce LFT approximation plot - Added small data files of curves for PCR and LFT to repository

--- run_analysis.R
+++ run_analysis.R
@@ -214,6 +214,16 @@ p_tab$diff <- rep(p_vals, rep(4000, length(p_vals)))
 p_tab$iter <- rep(1:4000, length(p_vals))
 p_tab[, variable := NULL]
 
+# Write PCR curve to csv
+fwrite(p_tab[, .(days_since_infection = diff, sample = iter, value)], file = "PCR_curve.csv")
+
+# Write PCR curve summary to csv
+fwrite(p_tab[, .(median = median(value), 
+                 lower_95 = quantile(value, 0.025), 
+                 upper_95 = quantile(value, 0.975)), 
+             by = list(days_since_infection = diff)],
+       file = "PCR_curve_summary.csv")
+
 # Incubation period distribution functions
 inc_cumulative <- function(x){plnorm(x, meanlog = dat$lmean, sdlog = dat$lsd) }
 inc_probability <- function(x){dlnorm(x, meanlog = dat$lmean, sdlog = dat$lsd) }
@@ -465,8 +475,100 @@ fig4b <- rbindlist(list(t3,t2), use.names = TRUE) %>%
   theme(legend.position = "bottom")
 
 # Put figure 4 together
-fig4a / fig4b + patchwork::plot_annotation(tag_levels = "A")
+figS1 <- fig4a / fig4b + patchwork::plot_annotation(tag_levels = "A")
 
+# Save figure S1
+ggsave(figS1, filename = "figureS1.pdf", height = 30, width = 40, units = "cm")
+
+###########################################
+# S3: LFT sensitivity curve approximation #
+###########################################
+
+# Create new synthetic PCR results with different ct threshold
+test_lft <- test_final[, pcr_result := ifelse(ct == 0 | is.na(ct), 0, ifelse(ct <= 28, TRUE, FALSE))]
+
+# Update data
+dat_lft <- dat
+
+dat_lft$test_result <- test_lft$pcr_result %>% as.numeric()
+dat_lft$te_upper_bound <- test_lft[, te_upper_bound := ifelse(
+  any(day[pcr_result == TRUE] < first_symp_day[pcr_result == TRUE]),
+  min(day[pcr_result == TRUE & day < first_symp_day]),
+  first_symp_day), by = num_id
+][, .(te_upper_bound = unique(te_upper_bound)), num_id][,te_upper_bound]
+
+fit_lft <- rstan::sampling(mod, chains = 4, 
+                       iter = 2000,
+                       warmup = 1000,
+                       data = dat_lft,
+                       seed = seedx,
+                       control = list(adapt_delta = 0.9, 
+                                      stepsize = 0.75,
+                                      max_treedepth = 13))
+res_lft <- rstan::extract(fit_lft)
+
+# Samples from LFT positive curve at different times since infection
+p_tab_lft <- as.data.table(res_lft$p)
+p_tab_lft <- data.table::melt(p_tab_lft)
+p_tab_lft$diff <- rep(p_vals, rep(4000, length(p_vals)))
+p_tab_lft$iter <- rep(1:4000, length(p_vals))
+p_tab_lft[, variable := NULL]
+
+# Write LFT curve to csv
+fwrite(p_tab_lft[, .(days_since_infection = diff, sample = iter, value)], file = "LFT_curve.csv")
+
+# Write PCR curve summary to csv
+fwrite(p_tab_lft[, .(median = median(value), 
+                 lower_95 = quantile(value, 0.025), 
+                 upper_95 = quantile(value, 0.975)), 
+             by = list(days_since_infection = diff)],
+       file = "LFT_curve_summary.csv")
+
+# LFT sensitivity plot
+p_lft <- data.frame(top = apply(res_lft$p, 2, quantile, prob = 0.975), 
+                bottom = apply(res_lft$p, 2, quantile, prob = 0.025),
+                y = apply(res_lft$p, 2, median),
+                days = seq(0, 30, 0.1)) %>%
+  ggplot2::ggplot(ggplot2::aes(x = days, y=y,  ymin = bottom, ymax = top, fill = "Posterior distribution")) +
+  ggplot2::geom_ribbon(alpha = 0.75) + 
+  cowplot::theme_cowplot() + 
+  ggplot2::geom_line(aes(lty = "Posterior median")) +
+  ggplot2::labs(y = "Probability of positive LFT (%)", x = "Days since infection") +
+  ggplot2::scale_y_continuous(breaks = seq(0, 1, 0.2), labels = paste0(seq(0, 100, 20))) +
+  ggplot2::scale_x_continuous(breaks = c(0, seq(5, 30, 5)))
+
+# Generate empirical distribution of PCR curve from posterior samples 
+# of infection times
+
+res_day_lft <- as.data.table(t(res_lft$T_e)) %>%
+  melt(value.name = "inf_day", variable.name = "iter")
+
+res_day_lft[ , num_id := 1:.N, iter]
+res_day_lft <- res_day_lft[, .(iter = 1:.N, inf_day), num_id]
+
+res_day_lft <- merge(test_lft, res_day_lft, by = "num_id", allow.cartesian = TRUE)
+
+res_day_lft[, x := day - round(inf_day)]
+
+res_day_lft <- res_day_lft[, .(pos = sum(pcr_result) / .N), by = list(iter, x)
+][, .(top = quantile(pos, 0.975), 
+      mean = mean(pos),
+      bottom = quantile(pos, 0.025)), by = x][x >= 0]
+
+# Add empirical distribution to posterior distribution plot
+pcols_lft <- c("Posterior distribution" = "firebrick2","Empirical Distribution" = "black")
+figS3 <- p_lft + 
+  geom_ribbon(data = res_day_lft, inherit.aes = FALSE, aes(x = x, y = mean, ymin = bottom, ymax = top, fill = "Empirical Distribution"), alpha = 0.25) +
+  geom_line(data = res_day_lft, inherit.aes = FALSE, aes(x = x, y = mean, lty = "Empirical mean")) +
+  scale_y_continuous(breaks = seq(0, 1, 0.2), labels = seq(0, 100, 20)) +
+  coord_cartesian(xlim = c(0, 30)) +
+  ggtitle("LFT positivity over the course of infection") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_fill_manual(values = pcols_lft, name = "") +
+  scale_linetype_discrete(name = "")
+
+# Save figure S3
+ggsave(figS3, filename = "figureS3.pdf", height = 15, width = 20, units = "cm")
 
 ########################
 # Sensitivity analysis #
