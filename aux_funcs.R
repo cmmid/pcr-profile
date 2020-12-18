9c9d132447b1c734f705943c4405cdc726b79600
Joel Hellewell <j.hellewell14@imperial.ac.uk>
2020-12-18 14:31:25 +0000

    Finished off supplementary plot function

--- aux_funcs.R
+++ aux_funcs.R
@@ -128,15 +128,102 @@ fit_different_ct <- function(ct_threshold) {
   
   # Add empirical distribution to posterior distribution plot
   pcols_lft <- c("Posterior distribution" = "firebrick2","Empirical Distribution" = "black")
-  figS3 <- p_lft + 
+  figS3b <- p_lft + 
     geom_ribbon(data = res_day_lft, inherit.aes = FALSE, aes(x = x, y = mean, ymin = bottom, ymax = top, fill = "Empirical Distribution"), alpha = 0.25) +
     geom_line(data = res_day_lft, inherit.aes = FALSE, aes(x = x, y = mean, lty = "Empirical mean")) +
     scale_y_continuous(breaks = seq(0, 1, 0.2), labels = seq(0, 100, 20)) +
     coord_cartesian(xlim = c(0, 30)) +
-    ggtitle(paste0("LFT positivity over the course of infection (ct threshold = ", ct_threshold,")")) +
+    ggtitle(paste0("LFT positivity over the course of infection")) +
     theme(plot.title = element_text(hjust = 0.5)) +
     scale_fill_manual(values = pcols_lft, name = "") +
     scale_linetype_discrete(name = "")
   
-  return(figS3)
+  figS3d <- ct_plot_dt %>%
+    ggplot(aes(x = x_date, y = ct, group = num_id)) + 
+    geom_point(aes(col = ifelse(ct >= ct_threshold, "Negative", "Positive"))) +
+    scale_y_reverse() +
+    cowplot::theme_cowplot() +
+    labs(y = "Ct value", x = "Days since infection") +
+    geom_vline(xintercept = 0, lty = 2) +
+    geom_hline(yintercept = ct_threshold, lty = 2) +
+    scale_color_manual(name = "Test result", values = c("black", "red")) +
+    ggtitle(paste0("Test results with ct value threshold of ", ct_threshold)) +
+    theme(legend.position = "right",
+          plot.title = element_text(hjust = 0.5))
+  
+  
+  # Evaluated testing frequencies
+  day_list <- c(2, 3, 4)
+  
+  # Samples from PCR positive curve at different times since infection
+  p_tab <- as.data.table(res_lft$p)
+  p_vals <- seq(0, 30, 0.1)
+  p_tab <- data.table::melt(p_tab)
+  p_tab$diff <- rep(p_vals, rep(12000, length(p_vals)))
+  p_tab$iter <- rep(1:12000, length(p_vals))
+  p_tab[, variable := NULL]
+  
+  # Create data table that calculate detection probabilities for different values
+  # the function detpr is in the file aux_funcs.R
+  tab <- data.table(every = rep(rep(day_list, rep(1, length(day_list))), 1),
+                    within = 30,
+                    delay = 0)
+  tab[, med := detpr(freqX = every, qu = 0.5, detect_within = within, delay_to_result = delay),
+      by = c("every", "within", "delay") ]
+  tab[, top :=  detpr(freqX = every, qu = 0.975, detect_within = within, delay_to_result = delay),
+      by = c("every", "within", "delay") ]
+  tab[, bottom :=  detpr(freqX = every, qu = 0.025, detect_within = within, delay_to_result = delay),
+      by = c("every", "within", "delay") ]
+  
+  tab[, every_lab := paste0("every ", every, " day(s)")]
+  tab$every_lab <- factor(tab$every_lab, levels = paste0("every ", day_list, " day(s)"))
+  tab$delay <- factor(tab$delay, labels = c("0 days"))
+  
+  fig3b <- tab %>%
+    ggplot(aes(x = every_lab, y = med, ymin = bottom, ymax = top)) +
+    geom_errorbar(position = position_dodge(0.5), width = 0.5) +
+    geom_point(position = position_dodge(0.5)) +
+    scale_color_brewer(palette = "Set1", name = "Results delay") +
+    cowplot::theme_minimal_hgrid() +
+    theme(legend.position = "right",
+          plot.title = element_text(hjust = 0.5)) +
+    labs(x = "Testing frequency", y = "Probability (%)", title = "Probability of detecting symptomatic case before onset") +
+    scale_y_continuous(breaks = seq(0, 1, 0.25), labels = seq(0, 100, 25)) +
+    coord_cartesian(ylim = c(0 ,1))
+  
+  # Same procedure as for 3A but with different probability
+  # of detection function
+  tab2 <- data.table(every = rep(rep(day_list, rep(length(1), length(day_list))), 1),
+                     within = 7,
+                     delay = 0)
+  tab2[, med := detpr2(freqX = every, qu = 0.5, detect_within = within, delay_to_result = delay), 
+       by = c("every", "within", "delay") ]
+  tab2[, top :=  detpr2(freqX = every, qu = 0.975, detect_within = within, delay_to_result = delay),
+       by = c("every", "within", "delay") ]
+  tab2[, bottom :=  detpr2(freqX = every, qu = 0.025, detect_within = within, delay_to_result = delay),
+       by = c("every", "within", "delay") ]
+  
+  tab2[, every_lab := paste0("every ", every, " day(s)")]
+  tab2$every_lab <- factor(tab2$every_lab, levels = paste0("every ", day_list, " day(s)"))
+  
+  # tab$within <- factor(tab$within, labels = c("Detection within 5 days", "Detection within 7 days"))
+  tab2$delay <- factor(tab2$delay, labels = c("0 days"))
+  
+  fig3c <- tab2 %>%
+    ggplot(aes(x = every_lab, y = med, ymin = bottom, ymax = top)) +
+    geom_errorbar(position = position_dodge(0.5), width = 0.5) +
+    geom_point(position = position_dodge(0.5)) +
+    scale_color_brewer(palette = "Set1", name = "Results delay") + 
+    cowplot::theme_minimal_hgrid() +
+    theme(legend.position = "right",
+          plot.title = element_text(hjust = 0.5)) +
+    # facet_wrap(~ within) +
+    labs(x = "Testing frequency", y = "Probability (%)", title = "Probability of detecting asymptomatic case within 7 days") +
+    scale_y_continuous(breaks = seq(0, 1, 0.25), labels = seq(0, 100, 25)) +
+    coord_cartesian(ylim = c(0 ,1))
+  
+  bot_panel <- (fig3b + fig3c) + patchwork::plot_layout(guides = "collect")
+  figure3 <- (figS3d + figS3b) / bot_panel + plot_annotation(tag_levels = "A") 
+  
+  return(figure3)
 }
